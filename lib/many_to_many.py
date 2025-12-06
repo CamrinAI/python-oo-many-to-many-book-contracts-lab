ae93d51be76e19cd5fe6c37a97bd9694354aad1a
Camrin <camrindavis58@gmail.com>
2025-12-05 22:14:09 -0600

    Implement Author, Book, and Contract classes with initialization and storage functionality. Add methods for managing relationships between authors and books through contracts. Update tests to validate new functionality.

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,30 @@
-class Author:
-    pass
+# * `__init__`: title
+# * Class attributes- all
+# * Methods:
+#   * contracts()- This method should return a list of related contracts
+#   * authors()- This method should return a list of related authors using the Contract class as an intermediary
 
+ from turtle import title
+from unicodedata import name
+
+
+class Author:
+    all = []
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
 
 class Book:
-    pass
-
+    all = []
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
